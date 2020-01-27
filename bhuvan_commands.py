360e8b7e07f0c2fefe67afaa2e3eb36118eeb4c1
Bhuvan <msbhuvanbhuvi@gmail.com>
2020-01-27 01:59:14 -0800

    The mode single-all needs further changes.

--- bhuvan_commands.py
+++ bhuvan_commands.py
@@ -2,3 +2,6 @@ Capstone Project Commands
 
 VM:
 bhuvanvm: ssh msbhuvan@13.64.190.226
+with_ssh_tunnel_for_Jupyter: ssh -N -f -L localhost:8889:localhost:8889 msbhuvan@13.64.190.226
+stop connection: sudo netstat -lpn |grep :YYYY # and kill the shown PID.
+starting jupyter: jupyter notebook --no-browser --port=8889 --allow-root
