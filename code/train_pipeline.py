ca043161a0ce24c1f9966f68b3dc54910d2a5d63
Bhuvan <msbhuvanbhuvi@gmail.com>
2020-02-22 02:04:31 -0800

    Update the inferenc etest notebook, add auc as monitoring metric.

--- code/train_pipeline.py
+++ code/train_pipeline.py
@@ -106,7 +106,7 @@ def train(train_metadata_file_path,
         raise ValueError("Since num_classes equals 1, the label_name must be provided.")
 
     train_data_epoch_subdivisions = 4
-    early_stop_monitor = "val_accuracy"
+    early_stop_monitor = "auc"
     early_stop_min_delta = 0.01
     early_stop_patience = patience * train_data_epoch_subdivisions  # One run through the train dataset.
     prefetch_buffer_size = 3  # Can be also be set to tf.data.experimental.AUTOTUNE
@@ -153,9 +153,9 @@ def train(train_metadata_file_path,
                                                        min_delta=early_stop_min_delta,
                                                        patience=early_stop_patience)
 
-    best_model_checkpoint_acc_callback = keras.callbacks.ModelCheckpoint(filepath=os.path.join(out_dir, "best_model_dir-acc.ckpt"),
+    best_model_checkpoint_auc_callback = keras.callbacks.ModelCheckpoint(filepath=os.path.join(out_dir, "best_model_dir-auc.ckpt"),
                                                                          mode='max',
-                                                                         monitor='val_accuracy',
+                                                                         monitor='auc',
                                                                          save_best_only=True,
                                                                          save_weights_only=False,
                                                                          verbose=1)
@@ -171,7 +171,7 @@ def train(train_metadata_file_path,
                                                        write_images=True)
 
     callbacks = [earlystop_callback,
-                 best_model_checkpoint_acc_callback,
+                 best_model_checkpoint_auc_callback,
                  best_model_checkpoint_loss_callback,
                  tensorboard_callback]
 
