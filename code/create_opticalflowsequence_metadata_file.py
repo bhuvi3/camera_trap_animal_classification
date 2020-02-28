bad0f39c45e5d0b361f7d11b311dd19a5872f7ba
Bhuvan <msbhuvanbhuvi@gmail.com>
2020-02-28 06:28:04 -0800

    Add function to update the metadata files.

--- code/create_opticalflowsequence_metadata_file.py
+++ code/create_opticalflowsequence_metadata_file.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 
 
-def create_optical_flow_metadata_file(src_metadata_file_path):
+def create_sequence_optical_flow_metadata_file(src_metadata_file_path):
     data_df = pd.read_csv(src_metadata_file_path)
 
     # Rename: image2 -> image3, image3 -> image5
@@ -20,11 +20,26 @@ def create_optical_flow_metadata_file(src_metadata_file_path):
     print("Optical Flow Sequence metadata file has been written to %s" % dest_metadata_file_path)
 
 
+def create_optical_flow_metadata_file(src_metadata_file_path):
+    data_df = pd.read_csv(src_metadata_file_path)
+
+    # Create: image2 as <sequence_opticalflowGF_1.png>, and image4 as <sequence__opticalflowGF_2.png>
+    data_df["opticalflowGF_1"] = data_df["sequence"].astype(str) + "_opticalflowGF_1.png"
+    data_df["opticalflowGF_2"] = data_df["sequence"].astype(str) + "_opticalflowGF_2.png"
+    data_df["opticalflowGF_average"] = data_df["sequence"].astype(str) + "_opticalflowGF_average.png"
+
+    # Save the new metadata file.
+    data_df_selected.to_csv(src_metadata_file_path, index=False)
+    print("Optical Flow Sequence metadata file has been overwritten to %s" % dest_metadata_file_path)
+
 if __name__ == "__main__":
     src_metadata_files = [
         "../data/final_dataset_train_balanced.csv",
         "../data/final_dataset_val_balanced.csv",
-        "../data/final_dataset_test_balanced-shuffled.csv"
+        "../data/final_dataset_test_balanced-shuffled.csv",
+        "../data/final_dataset_train-trial.csv",
+        "../data/final_dataset_val-trial.csv"
     ]
     for src_metadata_file in src_metadata_files:
-        create_optical_flow_metadata_file(src_metadata_file)
+        # create_sequence_optical_flow_metadata_file(src_metadata_file)
+        create_optical_flow_metadata_file(src_metadata_file_path)
